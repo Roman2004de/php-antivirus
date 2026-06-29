cb822199c8c1566f91ab0591717db145ff17ea6b
Roman Tarasenko <roman@d-element.ru>
2026-06-29 10:56:06 +0500

    Add finding suppression support

--- pushit.cmd
+++ pushit.cmd
@@ -2,5 +2,5 @@
 git status
 git remote set-url origin https://github.com/Roman2004de/php-antivirus
 git add .
-git commit -m "Add FindingSuppressor"
+git commit -m "Add finding suppression support"
 git push -u origin main
